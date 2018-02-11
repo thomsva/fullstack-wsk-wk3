3ea319a4b5ad9f7db5a40bd33c20bb298267f081
Thomas Svartbäck <thomas.svartback@helsinki.fi>
2018-02-11 11:35:47 +0200

    3.3 valmis

--- index.js
+++ index.js
@@ -41,7 +41,11 @@ app.get('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     console.log(id)
     const person = persons.find(person => person.id === id )
-    response.json(person)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
   })
 
 const PORT = 3001
